dce46eb8516e49b4bde731ad0fe585f106c03b6d
chaessi <chaessi514@gmail.com>
2026-01-12 20:39:16 +0900

    홈페이지구현

--- server.py
+++ server.py
@@ -1,9 +1,38 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
+
+
 @app.route('/')
 def index():
-    return "hi"
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>'''
+    
+@app.route('/hello/')
+def create():
+    return 'Create'
 
+@app.route('/read/<abc>')
+def read(abc):
+    print(abc)
+    return 'Read '+abc
 app.run(debug=True)
